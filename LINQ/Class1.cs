eabef3565b66c2b7c2002597a43bf3c16e75563c
opilane <opilane@HP-CZC0359NN7>
2023-03-08 10:45:37 +0200

    Add project files.

---
+++ LINQ/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class NumberList                     
+    {
+        public static readonly List<int> numberList = new List<int>()
+        {
+            0,1, 2, 3, 4, 5,
+        };
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ");
+
+            //kutsume WhereLINQ meetodi välja
+            WhereLINQ();
+            ThenByLINQ();
+            ThenByDescendingLINQ();
+            ToLookUpLINQ();
+            JoinLINQ();
+            SelectLINQ();
+            AllAndAnyLINQ();
+            ContainsLINQ();
+            AvarageLINQ();
+        }
+
+        public static void WhereLINQ()
+        {
+            var peopleAge = PeopleList.peoples
+                .Where(x => x.Age > 20 && x.Age < 23);
+
+            foreach (var item in peopleAge)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        public static void ThenByLINQ()
+        {
+            Console.WriteLine("ThenBy järgi reastamine");
+
+            var thenByResult = PeopleList.peoples
+                //järjestab nimede järgi ja siis vanuse järgi
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Age);
+
+            foreach (var people in thenByResult)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+
+        public static void ThenByDescendingLINQ()
+        {
+            Console.WriteLine("ThenByDescending");
+
+            var thenByDescending = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenByDescending(x => x.Age);
+
+            foreach (var people in thenByDescending)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+
+        public static void ToLookUpLINQ()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("ToLookUp järgi reastamine");
+
+            var toLookUp = PeopleList.peoples
+                .ToLookup(x => x.Age);
+
+            foreach (var people in toLookUp)
+            {
+                Console.WriteLine("Age group " + people.Key);
+
+                foreach (People p in people)
+                {
+                    Console.WriteLine("Person name " + p.Name);
+                }
+            }
+        }
+
+        public static void JoinLINQ()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("InnerJoin in LINQ");
+
+            var innerJoin = PeopleList.peoples
+                .Join(GenderList.genderList,
+                humans => humans.GenderId,
+                gender => gender.Id,
+                (humans, gender) => new
+                {
+                    Name = humans.Name,
+                    Sex = gender.Sex
+                });
+
+            foreach (var people in innerJoin)
+            {
+                Console.WriteLine(people.Name + " " + people.Sex);
+            }
+        }
+
+        public static void GroupJoinLINQ()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            var groupJoin = GenderList.genderList
+                .GroupJoin(PeopleList.peoples,
+                p => p.Id,
+                g => g.GenderId,
+                (p, peopleGroup) => new
+                {
+                    Humans = peopleGroup,
+                    GenderFullName = p.Sex
+                });
+
+            foreach (var people in groupJoin)
+            {
+                Console.WriteLine(people.GenderFullName);
+
+                foreach (var name in people.Humans)
+                {
+                    Console.WriteLine(name.Name);
+                }
+            }
+        }
+
+        public static void SelectLINQ()
+        {
+            Console.WriteLine("Select in LINQ");
+            //teha iseseisvalt LINQ Select 
+            //otsida Internetist vastuseid
+            // Create a list of integers
+            var vals = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+            var res = vals.Select(e => e * 2);
+            Console.WriteLine(string.Join(',', res));
+        }
+
+        public static void AllAndAnyLINQ()
+        {
+            Console.WriteLine("All LINQ");
+
+            bool areAllPeopleTeenagers = PeopleList.peoples
+                .All(x => x.Age > 18);
+
+
+
+            Console.WriteLine(areAllPeopleTeenagers);
+
+            Console.WriteLine("Any LINQ");
+            bool isAnyPersonTeenager = PeopleList.peoples
+                .Any(x => x.Age > 18);
+
+            Console.WriteLine(isAnyPersonTeenager);
+        }
+ 
+        public static void ContainsLINQ()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Contains LINQ");
+
+            bool result = NumberList.numberList.Contains(10);
+
+            Console.WriteLine(result);
+        }
+    
+        public static void AggregateLINQ()
+        {
+            string commaSeparatedPersonNames = PeopleList.peoples
+                .Aggregate<People, string>(
+                "People names: ",
+                (str, x) => str += x.Name + ", "
+                );
+
+            Console.WriteLine(commaSeparatedPersonNames);
+        }
+
+        public static void AvarageLINQ()
+        {
+            Console.WriteLine("Avarage LINQ");
+            var avarageResult = PeopleList.peoples
+                .Average(x => x.Age);
+
+            Console.WriteLine(avarageResult);
+        }
+    }   
+}
